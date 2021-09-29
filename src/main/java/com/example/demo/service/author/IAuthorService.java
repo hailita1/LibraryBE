829f38d16cf23821c796241ba2d85332bcad6737
hailita1 <hailit2306@gmail.com>
2021-09-29 19:22:11 +0700

    create service

---
+++ src/main/java/com/example/demo/service/author/IAuthorService.java
@@ -0,0 +1,4 @@
+package com.example.demo.service.author;
+
+public interface IAuthorService {
+}
---
+++ src/main/java/com/example/demo/service/publishing_company/IPublishingCompanyService.java
@@ -0,0 +1,4 @@
+package com.example.demo.service.publishing_company;
+
+public interface IPublishingCompanyService {
+}
